d9a9a710633da395def08331f32ff0418f19bcea
Yerramsetti, Harisri <hyerramsetti3@gatech.edu>
2024-06-17 23:07:17 -0400

    Should pass

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../component/todos.css";
-import { Card, Grid, ListItemText, ListItemButton, Checkbox} from "@mui/material";
+import { Card, CardContent, Grid, ListItemButton, ListItemText, Checkbox } from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -10,22 +10,16 @@ import { Card, Grid, ListItemText, ListItemButton, Checkbox} from "@mui/material
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
 const Todos = ({ todos, deleteTodo }) => {
-  const doDelete = (id) => {
-    deleteTodo(id);
-  };
-
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
         <Grid key={todo.id}>
           <Card style={{marginTop:10}}>
+            {/* Remember, we set the local state of this todo item when the user submits the form in 
+            AddTodo.js. All we need to do is return the todo list item {todo.content} */}
             <ListItemButton component="a" href="#simple-list">
-              <Checkbox 
-              color="primary" 
-              style={{ paddingLeft:0 }} 
-              onClick={() => doDelete(todo.id)} 
-              />
-              <ListItemText primary={todo.content} secondary={new Date(todo.date).toLocaleString('en-US')}/>
+              <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
+              <ListItemText primary={todo.content} secondary={todo.date}/>
             </ListItemButton>
           </Card>
         </Grid>
